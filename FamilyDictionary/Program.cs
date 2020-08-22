37203448701a9cb889a9f0073221d014cfb76fc9
Anca Simon <ancarsimon@gmail.com>
2020-08-22 13:41:18 -0500

    Updates based on class discussion.

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -14,30 +14,36 @@ namespace FamilyDictionary
             myFamily.Add("middle brother", new Dictionary<string, string>() { { "name", "Bogdan" }, { "age", "44" } });
             myFamily.Add("mother", new Dictionary<string, string>() { { "name", "Maria" }, { "age", "75" } });
 
-            //just to test - the following way to initialize the Dictionary items works ... :
-            //myFamily.Add("grandfather", new Dictionary<string, string>() { { "Pavel", "92" } });
 
             foreach (var (relation, details) in myFamily)
             {
-                //string relationDetails = string.Join(",", details);
-                //Console.WriteLine($"{relationDetails} is my {relation} and is {relationDetails} years old.");
                 string relativeName = "";
                 string relativeAge = "";
 
                 foreach (var (entry1, entry2) in details)
                 {
-                    if (entry1 == "name")
+                    //if (entry1 == "name")
+                    if(entry1.Equals("name", StringComparison.OrdinalIgnoreCase)) //Anca: better option because thins finds "name" regardless of its capitalization!!
                     {
                         relativeName = entry2;
                     }
-                    else if (entry1 == "age")
+                    //else if (entry1 == "age")
+                    else if(entry1.Equals("age", StringComparison.OrdinalIgnoreCase))
                     {
                         relativeAge = entry2;
                     }
                 }
                 Console.WriteLine($"{relativeName} is my {relation} and is {relativeAge} years old.");
-
             }
+
+            //Another option: --note that the key names have to be exact in this case- so if one of the items had "Name" instead of "name", it would not work!
+            foreach (var (relation, details) in myFamily)
+            {
+                Console.WriteLine($"{details["name"]} is my {details["age"]}-year old {relation}.");
+            }
+
+            //and another option with .Select()!!
+            myFamily.Select((role) => $"{role.Value["name"]} is my {role.Value["age"]} year old {role.Key}!!!!");
         }
     }
 }
